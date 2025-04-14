e0b896d7817d6540a7a6282b44fc2df39921b5ed
Tushar Vashishth <tusharvashishth469@gmail.com>
2025-04-14 17:16:29 +0530

    New Updation

--- basicreactapp/src/App.jsx
+++ basicreactapp/src/App.jsx
@@ -1,10 +1,14 @@
-import "./App.css"
-import Title from "./Title.jsx";
+import "./App.css";
 import ProductTab from "./ProductTab.jsx";
 
 
 function App() {
-  return <ProductTab/>;
+  return (
+    <>
+      <h2>Blockbuster Deals | Shop Now </h2>
+      <ProductTab />
+    </>
+  );
 }
 
 export default App
--- basicreactapp/src/Product.jsx
+++ basicreactapp/src/Product.jsx
@@ -1,12 +1,23 @@
 import "./Product.css";
+import Price from "./Price.jsx";
 
-function Product(){
-    return (
-        <div className = "Product">
-            <h3>Product Title</h3>
-            <h3>Product Description</h3>
-        </div>
-    );
+function Product({ title, idx }) {
+  let oldPrices = ["12,345", "11,999", "2499", "599"];
+  let newPrices = ["11,399", "8,999", "1599", "349"];
+  let description = [
+    ["8,000 DPI", "5 Programmable buttons"],
+   [ "intutive surface", "designed for the ipad pro"],
+    ["designed for ipad pro", "intutive surface"],
+    ["wireless", "optical orientation"],
+  ];
+  return (
+    <div className="Product">
+      <h4>{title}</h4>
+      <p>{description[idx][0]}</p>
+      <p>{description[idx][1]}</p>
+      <Price oldPrice={oldPrices[idx]} newPrice={newPrices[idx]} />
+    </div>
+  );
 }
 
 export default Product;
