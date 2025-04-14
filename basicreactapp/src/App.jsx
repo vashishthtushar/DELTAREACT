acc737f438921f429593f6c6e1b4569a26d15fb4
Tushar Vashishth <tusharvashishth469@gmail.com>
2025-04-14 10:56:33 +0530

    first commit

---
+++ basicreactapp/src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css"
+import Title from "./Title.jsx";
+import ProductTab from "./ProductTab.jsx";
+
+
+function App() {
+  return <ProductTab/>;
+}
+
+export default App
---
+++ basicreactapp/src/Title.jsx
@@ -0,0 +1,13 @@
+function Title() {
+    let name = "Tushar"
+  return (
+    <div>
+      <p>2*2 = {2*2}</p>
+      <p>hi, {name.toUpperCase()}</p>
+    </div>
+  );
+}
+
+export default Title;
+
+// export {Title};
